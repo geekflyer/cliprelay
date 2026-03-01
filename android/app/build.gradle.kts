379c5adf75e2a8e4f894a44417b5a6bcb16ad36e
geekflyer <c.theilemann@gmail.com>
2026-03-01 08:07:35 -0800

    chore(android): bump version code to 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         applicationId = "com.cliprelay"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
     }
 
